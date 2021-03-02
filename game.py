8c10375d5a7f6efbb99fa82ad1de8d618773c679
Vladislav Korecký <vladislav.ml@korecky.org>
2021-03-02 21:34:12 +0100

    Handling player input

--- game.py
+++ game.py
@@ -1,8 +1,20 @@
 class Game:
-    def __init__(self, y_size, x_size):
+    def __init__(self, y_size, x_size, players):
         self.board = []
+        self.players = players
+
         for y in range(y_size):
             self.board.append([])
 
             for x in range(x_size):
                 self.board[y].append(0)
+
+    def play(self):
+        for player in self.players:
+            player_input = input()
+            player_input = player_input.split(",")
+
+            for i in range(len(player_input)):
+                player_input[i] = int(player_input[i])
+
+            self.board[player_input[1]][player_input[0]] = player.player_num + 1
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from game import *
+from player import *
 
-game = Game(6, 6)
+game = Game(6, 6, [Player(0), Player(1)])
 for y in game.board:
     print(y)
+game.play()
