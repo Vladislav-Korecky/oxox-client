da79e76f146cba3020a5afafbfdf23c87a02f244
Vladislav Korecký <vladislav.ml@korecky.org>
2021-03-07 09:25:33 +0100

    Adding comments and clening code

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 class Game:
     def __init__(self, y_size, x_size, players):
+        # init
         self.board = []
         self.players = players
 
+        # creating board
         for y in range(y_size):
             self.board.append([])
 
@@ -12,35 +14,39 @@ class Game:
     def play(self):
         player_index = 0
 
+        # game loop
         while True:
-            for line in self.board:
-                print(line)
+            # printing board state
+            self.print_board()
 
-            player_input = input()
-            player_input = player_input.split(",")
+            # getting tile position from player
+            player_input = self.get_input()
 
-            for i in range(len(player_input)):
-                player_input[i] = int(player_input[i])
-
+            # updating board
             if self.board[player_input[1]][player_input[0]] == 0:
                 self.board[player_input[1]][player_input[0]] = self.players[player_index].player_num
             else:
                 continue
 
-            if self.__is_win(player_input[0], player_input[1], player_index):
+            # checking for win
+            if self.is_win(player_input[0], player_input[1], player_index):
                 return player_index
 
+            # giving turn to another player
             player_index += 1
 
             if player_index == len(self.players):
                 player_index = 0
 
-    def __is_win(self, x, y, player_index):
+    def is_win(self, x, y, player_index):
+        # checking four directions (vectors)
         for i in range(4):
+            # init
             vector = []
             x2, y2 = x, y
             player_index2 = player_index
 
+            # selecting vector
             if i == 0:
                 vector = [1, 0]
             elif i == 1:
@@ -50,42 +56,72 @@ class Game:
             elif i == 3:
                 vector = [-1, 1]
 
+            # checking tiles
             while True:
+                # changing player
                 player_index2 += 1
 
                 if player_index2 == len(self.players):
                     player_index2 = 0
 
-                if y2 < 0 or y2 >= len(self.board):
+                # checking if tile exists
+                if self.tile_exists(x2, y2):
                     break
 
-                if x2 < 0 or x2 >= len(self.board[y2]):
-                    break
-
+                # checking if tile belongs to player
                 if self.board[y2][x2] != self.players[player_index2].player_num:
                     break
 
+                # changing tile
                 x2 += vector[0]
                 y2 += vector[1]
 
+            # checking direction in reverse for signs of winning combination
             for j in range(4):
+                # changing player
                 player_index2 += 1
+
                 if player_index2 == len(self.players):
                     player_index2 = 0
 
-                if y2 < 0 or y2 >= len(self.board):
+                # checking if tile exists
+                if not self.tile_exists(x2, y2):
                     break
 
-                if x2 < 0 or x2 >= len(self.board[y2]):
-                    break
-
+                # checking if tile belongs to player
                 if self.board[y2][x2] != self.players[player_index2].player_num:
                     break
 
+                # checking for end of pattern
                 if j == 3:
                     return True
 
+                # changing tile
                 x2 -= vector[0]
                 y2 -= vector[1]
 
         return False
+
+    def print_board(self):
+        for line in self.board:
+            print(line)
+
+    def get_input(self):
+        player_input = input().split(",")
+
+        processed_input = []
+        for i in range(len(player_input)):
+            processed_input.append(int(player_input[i]))
+
+        return processed_input
+
+    def tile_exists(self, x, y):
+        # checking y axis
+        if y < 0 or y >= len(self.board):
+            return False
+
+        # checking x axis
+        if x < 0 or x >= len(self.board[y]):
+            return False
+
+        return True
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from game import *
 from player import *
 
+# creating game
 game = Game(6, 6, [Player(1), Player(2)])
+
+# starting new match
 winner = game.play()
+
+# printing winner
 print("Player number " + str(winner) + " won!")
