8b10474845000a3995eaae100f6bd67946c122db
Vladislav Korecký <vladislav.ml@korecky.org>
2021-03-28 17:13:15 +0200

    Adding comments

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ from game import *
 from player import *
 
 
+# board button
 class BoardButton(Button):
     pass
 
 
+# board layout
 class BoardLayout(GridLayout):
     def __init__(self, board, **kwargs):
         super(BoardLayout, self).__init__(**kwargs)
@@ -23,34 +25,39 @@ class BoardLayout(GridLayout):
                 self.add_widget(BoardButton())
 
 
+# base layout
 class BaseLayout(FloatLayout):
     pass
 
 
+# app
 class OXOXApp(App):
     def __init__(self, board, **kwargs):
         super().__init__(**kwargs)
         self.board = board
 
+        # defining layouts
         self.base_layout = None
         self.board_layout = None
 
     def build(self):
-        Window.maximize()
-        Window.clearcolor = (1, 1, 1, 1)
-
+        # setting layouts
         self.base_layout = BaseLayout()
-
         self.board_layout = BoardLayout(self.board)
 
+        # editing window
+        Window.maximize()
+        Window.clearcolor = (1, 1, 1, 1)
         Window.bind(on_resize=self.on_resize)
 
+        # calling on_resize to center widgets
         self.on_resize(Window, Window.size[0], Window.size[1])
 
         self.base_layout.add_widget(self.board_layout)
         return self.base_layout
 
     def on_resize(self, window, width, height):
+        # centering board
         x_pos = (width - (75 * len(self.board))) / 2
         self.board_layout.pos_hint = {"y": -0.3}
         self.board_layout.pos = (x_pos, 0)
